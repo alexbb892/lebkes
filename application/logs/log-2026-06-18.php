46bae74dc0c04c8ffbefa4a88ccda50b986d2be2
alexbb892 <alexbb892@gmail.com>
2026-06-18 22:10:24 +0700

    penambahan gamabr doctor_patient,png

---
+++ application/logs/log-2026-06-18.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-06-18 21:59:51 --> 404 Page Not Found: Auth/login
+ERROR - 2026-06-18 22:00:19 --> 404 Page Not Found: Assets/img
+ERROR - 2026-06-18 22:02:32 --> 404 Page Not Found: Auth/login
+ERROR - 2026-06-18 22:02:41 --> 404 Page Not Found: Auth/login
+ERROR - 2026-06-18 22:02:47 --> 404 Page Not Found: Auth/login
+ERROR - 2026-06-18 22:02:53 --> 404 Page Not Found: Assets/img
+ERROR - 2026-06-18 22:06:03 --> 404 Page Not Found: Assets/img
+ERROR - 2026-06-18 22:06:03 --> 404 Page Not Found: Dashboard/get_demographic_data
+ERROR - 2026-06-18 22:07:41 --> 404 Page Not Found: Dashboard/get_demographic_data
